68f8b0b5c5675e055280182e6388f47e298789d0
tanyasangal21 <tanya.sangal2013@vit.ac.in>
2016-09-20 18:22:01 +0530

    Create get_city.php

---
+++ get_city.php
@@ -0,0 +1,19 @@
+
+
+<?php
+include('Dbconfig.php');
+if($_POST['name'])
+{
+	$id=$_POST['name'];
+	
+	$stmt = $DB_con->prepare("SELECT * FROM cities WHERE city_state='$id'");
+	$stmt->execute(array(':name' => $name));
+	?><option selected="selected">Select City :</option>
+	<?php while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+	{
+		?>
+		<option value="<?php echo $row['city_id']; ?>"><?php echo $row['city_name']; ?></option>
+		<?php
+	}
+}
+?>
