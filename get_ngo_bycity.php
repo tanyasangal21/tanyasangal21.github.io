b6f287ebd670c900a58f73e5778a63dd0f7397b2
tanyasangal21 <tanya.sangal2013@vit.ac.in>
2016-09-21 20:02:31 +0530

    Update get_ngo_bycity.php

--- get_ngo_bycity.php
+++ get_ngo_bycity.php
@@ -15,14 +15,15 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-if(isset($str))
+if(isset($str) &&$str!='--Select City--')
 {
 	if(empty($str2)&& empty($str3)){
 	$sql = "SELECT * FROM ngo natural join cities where cities.city_name=ngo.city and cities.city_id=".$str."";
 }
 }
-else if($str=='--Select City--')
+if($str=='--Select City--')
 {	
+ 
 	if(isset($str2) && empty($str3) )
 	{
 		$sql = "SELECT * FROM ngo where name='".$str2."'";
@@ -39,7 +40,7 @@ else if($str=='--Select City--')
 	
 	if (!$result || mysqli_num_rows($result)==0)
 	{
-		echo "NO NGO'S" ;
+		echo '<p style="color:red; font-size:50px;margin-top:20%;"><i>No NGO Found!! <br> Please Check The Information Entered</i></p>' ;
 	}
 	else{	
 		echo'<table border="1" style="border-collapse:collapse; text-align:center; border:3px solid #558C89; width: 100%;" class="table-bordered">';
@@ -47,6 +48,7 @@ else if($str=='--Select City--')
 	    echo'<th style=" background-color: #558C89;color: white; text-align:center; padding: 10px;">ID</th>';
 		echo'<th style=" background-color: #558C89;color: white; text-align:center;padding: 10px;">Name</th>';
 		echo'<th style=" background-color: #558C89;color: white; text-align:center;padding: 10px;">Address</th>';
+        echo'<th style=" background-color: #558C89;color: white; text-align:center;padding: 10px;">Sector</th>';
 		
 	 echo'</tr>';
 while($row = mysqli_fetch_assoc($result)) {
@@ -54,7 +56,7 @@ while($row = mysqli_fetch_assoc($result)) {
 			echo'<td style="border-bottom: 1px solid #ddd;border-bottom: 1px solid #558C89 ; padding: 10px;">'.$row['id'].'</td>';
 			echo'<td style="border-bottom: 1px solid #ddd;border-bottom: 1px solid #558C89 ; padding: 10px;">'.$row['name'].'</td>';
 			echo'<td style="border-bottom: 1px solid #ddd;border-bottom: 1px solid #558C89 ; padding: 10px;">'.$row['address'].'</td>';
-			
+			echo'<td style="border-bottom: 1px solid #ddd;border-bottom: 1px solid #558C89 ; padding: 10px;">'.$row['sector'].'</td>';
 		echo '</tr>';
 }
 }  
